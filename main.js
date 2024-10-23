e8802ebc1f96d4713318703038bcd08d22b0419e
Lakshya Saxena <157016829+LakshyaSaxena49@users.noreply.github.com>
2024-10-24 01:28:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer.", "Web Developer.","UI/UX designer","Guitarist"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
